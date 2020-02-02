c860d5ec28f78f2288a65a7d296523709421ddec
codeinsack <codeinsack@gmail.com>
2020-02-02 19:18:12 +0300

    add new quote

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <app-new-quote @quotedAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
   import QuoteGrid from "./components/QuoteGrid.vue"
+  import NewQuote from "./components/NewQuote.vue";
 
   export default {
     data() {
@@ -14,8 +16,14 @@
         maxQuotes: 10,
       }
     },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote)
+      }
+    },
     components: {
       'app-quote-grid': QuoteGrid,
+      'app-new-quote': NewQuote,
     }
   }
 </script>
